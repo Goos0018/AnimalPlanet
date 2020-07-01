bff537b331c8fc130220560fb2519f89ec8776f7
Thomas Goos <goosthomas@gmail.com>
2020-07-01 08:31:48 +0200

    completed Mammalzoo and Protozoazoo

--- src/be/vdab/zoo/MammalZoo.java
+++ src/be/vdab/zoo/MammalZoo.java
@@ -1,18 +1,75 @@
 package be.vdab.zoo;
 
 import be.vdab.Animal;
+import be.vdab.Mammal;
 import be.vdab.mammal.Cow;
 import be.vdab.mammal.Monkey;
 import be.vdab.mammal.Tiger;
 
-public class MammalZoo extends Zoo{
+public class MammalZoo extends Zoo {
 
     Cow[] cows;
     Monkey[] monkeys;
     Tiger[] tigers;
 
+    public MammalZoo(Cow[] cows, Monkey[] monkeys, Tiger[] tigers) {
+        this.cows = cows;
+        this.monkeys = monkeys;
+        this.tigers = tigers;
+    }
+
+    public Cow[] getCows() {
+        return cows;
+    }
+
+    public void setCows(Cow[] cows) {
+        this.cows = cows;
+    }
+
+    public Monkey[] getMonkeys() {
+        return monkeys;
+    }
+
+    public void setMonkeys(Monkey[] monkeys) {
+        this.monkeys = monkeys;
+    }
+
+    public Tiger[] getTigers() {
+        return tigers;
+    }
+
+    public void setTigers(Tiger[] tigers) {
+        this.tigers = tigers;
+    }
+
     @Override
     public void addAnimal(Animal animal) {
+        if (!(animal instanceof Mammal)) {
+            System.out.println("this is not a mammal");
+            return;
+        } else if (animal instanceof Cow) {
+            Cow[] result = new Cow[cows.length + 1];
+            for (int i = 0; i < cows.length; i++) {
+                result[i] = cows[i];
+            }
+            result[result.length - 1] = (Cow) animal;
+            cows = result;
 
+        } else if (animal instanceof Monkey) {
+            Monkey[] result = new Monkey[monkeys.length + 1];
+            for (int i = 0; i < monkeys.length; i++) {
+                result[i] = monkeys[i];
+            }
+            result[result.length - 1] = (Monkey) animal;
+            monkeys = result;
+
+        } else if (animal instanceof Tiger) {
+            Tiger[] result = new Tiger[tigers.length + 1];
+            for (int i = 0; i < tigers.length; i++) {
+                result[i] = tigers[i];
+            }
+            result[result.length - 1] = (Tiger) animal;
+            tigers = result;
+        }
     }
 }
--- src/be/vdab/zoo/ProtozoaZoo.java
+++ src/be/vdab/zoo/ProtozoaZoo.java
@@ -1,6 +1,7 @@
 package be.vdab.zoo;
 
 import be.vdab.Animal;
+import be.vdab.Protozoa;
 import be.vdab.protozoa.Amoeba;
 import be.vdab.protozoa.Malaria;
 
@@ -9,8 +10,47 @@ public class ProtozoaZoo extends Zoo {
     Amoeba[] amoebas;
     Malaria[] malarias;
 
+    public ProtozoaZoo(Amoeba[] amoebas, Malaria[] malarias) {
+        this.amoebas = amoebas;
+        this.malarias = malarias;
+    }
+
+    public Amoeba[] getAmoebas() {
+        return amoebas;
+    }
+
+    public void setAmoebas(Amoeba[] amoebas) {
+        this.amoebas = amoebas;
+    }
+
+    public Malaria[] getMalarias() {
+        return malarias;
+    }
+
+    public void setMalarias(Malaria[] malarias) {
+        this.malarias = malarias;
+    }
+
     @Override
     public void addAnimal(Animal animal) {
+        if (!(animal instanceof Protozoa)) {
+            System.out.println("this is not a protozoa");
+            return;
+        } else if (animal instanceof Amoeba) {
+            Amoeba[] result = new Amoeba[amoebas.length + 1];
+            for (int i = 0; i < amoebas.length; i++) {
+                result[i] = amoebas[i];
+            }
+            result[result.length - 1] = (Amoeba) animal;
+            amoebas = result;
 
+        } else if (animal instanceof Malaria) {
+            Malaria[] result = new Malaria[malarias.length + 1];
+            for (int i = 0; i < malarias.length; i++) {
+                result[i] = malarias[i];
+            }
+            result[result.length - 1] = (Malaria) animal;
+            malarias = result;
+        }
     }
 }
